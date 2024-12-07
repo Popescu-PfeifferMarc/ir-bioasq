8646c48645115c9a4619aa4fe63d509f8be3240e
Marc Popescu-Pfeiffer <marc.popescu-pfeiffer@students.uni-mannheim.de>
2024-12-07 13:46:30 +0100

    tfidf model cleanup

---
+++ model_tfidf_create.py
@@ -0,0 +1,85 @@
+import logging
+import os.path
+import sys
+import csv
+import pprint
+import pickle
+
+from gensim.corpora import Dictionary
+from gensim.models import TfidfModel
+from gensim.utils import simple_preprocess
+from gensim.similarities import SparseMatrixSimilarity
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+# Config
+DICT_SIZE = 100_000
+input_file = "./dataset/pubmed_annual_baseline.csv"
+include_abstract = True
+output_base_folder = "./out/"
+smartirs = "ntc" # alternatives are "ltc" and "Ltc"
+
+# Computed config
+output_smartirs_name = "large_ltc" if smartirs == "Ltc" else "small_ltc" if smartirs == "ltc" else smartirs # special case for Ltc and ltc to avoid casing
+output_folder = output_base_folder + "tfidf_title/" if not include_abstract else output_base_folder + "tfidf_title_abstract/"
+
+# Setup
+program = os.path.basename(sys.argv[0])
+logger = logging.getLogger(program)
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
+logging.root.setLevel(level=logging.INFO)
+logger.info("running %s", ' '.join(sys.argv))
+os.makedirs(os.path.dirname(output_folder), exist_ok=True)
+
+# Corpus
+class DocumentCorpus:
+    def __iter__(self):
+        with open(input_file, mode='r', newline='', errors='ignore') as inp_file:
+            reader = csv.reader(inp_file)
+            for row in reader:
+                # doc_pmid = row[0]
+                doc_title = row[1]
+                doc_abstract = row[2]
+                if include_abstract:
+                    yield str(doc_title) + '\n' + str(doc_abstract)
+                else:
+                    yield str(doc_title)
+
+# Pre-process
+nltk.download('wordnet')
+nltk.download('stopwords')
+lemmatizer = WordNetLemmatizer()
+stoplist = set(stopwords.words('english'))
+def preprocess(input: str):
+    return [lemmatizer.lemmatize(entry) for entry in simple_preprocess(input) if entry not in stoplist]
+
+# Dictionary
+corpus = DocumentCorpus()
+dictionary = Dictionary(preprocess(doc) for doc in corpus)
+dictionary.filter_extremes(no_below=5, no_above=0.5, keep_n=DICT_SIZE)
+dictionary.compactify()
+dictionary.save_as_text(output_folder + '.dictionary')
+logger.info("Created dict")
+"""
+dictionary = Dictionary.load_from_text(outp + '.dictionary')
+logger.info("Loaded dictionary")
+"""
+
+# BOW corpus
+class BowCorpus:
+    def __iter__(self):
+        corpus = DocumentCorpus()
+        for doc in corpus:
+            yield dictionary.doc2bow(preprocess(doc))
+
+# TF-IDF model
+bow_corpus = BowCorpus()
+tfidf = TfidfModel(bow_corpus, smartirs=smartirs)
+tfidf.save(output_folder + '.model_tfidf_' + output_smartirs_name)
+logger.info("----- Created TfidfModel with smartirs: %s", smartirs)
+
+index = SparseMatrixSimilarity(tfidf[bow_corpus], num_features=len(dictionary))
+index.save(output_folder + '.simmat_tfidf_' + output_smartirs_name)
+logger.info("----- Created SparseMatrixSimilarity with smartirs: %s", smartirs)
